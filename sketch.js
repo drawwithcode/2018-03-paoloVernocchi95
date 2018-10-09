5f879fbeebb55582d97f41c021bcf88ea1b4b66c
paoloVernocchi95 <43385409+paoloVernocchi95@users.noreply.github.com>
2018-10-09 21:30:12 +0200

    martedì mattina

--- sketch.js
+++ sketch.js
@@ -7,9 +7,9 @@ var colorList = ['#e42b1d',//red
                  '#0073b4'];//blue
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
   textSize(32);
-  frameRate(30);
+  frameRate(60);
 
   /*translate(windowWidth/2, windowHeight/2);
 
@@ -28,10 +28,19 @@ function setup() {
 
 
 function draw() {
-translate(windowWidth/2, windowHeight/2);
-  var x = random() *400;
-var y = random() * 400;
-var r = random() * 10;
+  if(frameCount<2500){
+    fill(275);
+  text("Wait for the bubble pool to fill",200,800);
+
+  var x = random() *windowWidth;
+var y = random() * windowHeight;
+var a = random() *windowWidth;
+var b = random() * windowHeight;
+var g = random() *windowWidth;
+var f = random() * windowHeight;
+var h = random() *windowWidth;
+var i = random() * windowHeight;
+//var d = random() * 10;
 
 if(random()<0.25){
    fill(colorList[0]);
@@ -46,12 +55,50 @@ else  {
   fill(colorList[3]);
 }
 
+//|| (x>1200 && x <1720 && y>600 && y<800 )
+//diamentro
+if(x <windowWidth/6  && y<windowHeight/6 || x>(windowWidth*5)/6  && y>(windowHeight*5)/6 ){
+   var d=75;
+}
+/*else if (x >200 && x<600 && y>240 && y<480){
+  var d=62;
+}
+else if (x>1200 && x <1720 && y>600 && y<800 ) {
+var d=62;
+}*/
+else  {
+ var d=40;
+}
+
 
 stroke(1);
-ellipse(x, y, 25);
+ellipse(x, y, d);
+ellipse(y, x, d);
+ellipse(a, b, d);
+ellipse(b, a, d);
+ellipse(h, i, d);
+ellipse(i, h, d);
+
+}
+else if (frameCount>2500 && frameCount<2600) {
+  fill(colorList[0]);
+  ellipse(200, 800, 50);
+  fill(colorList[1]);
+  ellipse(250, 800, 50);
+  fill(colorList[2]);
+  ellipse(300, 800, 50);
+  fill(colorList[3]);
+  ellipse(350, 800, 50);
+}
+else  {
+textSize(42);
+text("Now Find Sheldon",200,840);
+
+}
+
   // put drawing code here
-  fill(0);
-  text(frameCount,windowWidth/2, windowHeight/2);
+  fill(275);
+  text(frameCount,300,300);
 
 
 }
